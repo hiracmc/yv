159680495b7737d6b5658be8a0954a6f07bda42e
hiracmc <120374767+hiracmc@users.noreply.github.com>
2025-07-17 14:32:50 +0900

    Delete src directory

--- src/shared/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    DISK_MOUNT_PATH: process.env.DISK_MOUNT_PATH || './tmp', // ローカル実行時はtmpフォルダ
-    INVIDIOUS_INSTANCE_URL: 'https://invidious.nikkosphere.com',
-    JOB_CHANNEL: 'video-jobs', // RedisのPub/Subチャンネル名
-    JOB_KEY_PREFIX: 'job:', // Redisのジョブ状態キー
-};
